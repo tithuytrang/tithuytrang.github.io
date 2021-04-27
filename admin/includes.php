b65f18baccce95d644a26a49365d659700b3da7e
Trang Truong <tithuytrang@gmail.com>
2021-04-27 22:07:02 +1000

    Up

--- admin/includes.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once "../res/x5engine.php";
-require_once "libraries/controlpanel.class.php";
--- admin/templates/dashboard/user-row.php
+++
@@ -1,5 +0,0 @@
-<div class="user text-small">
-	<span class="text-semibold"><?php echo $name ?></span>
-	<span class="text-light"><?php echo date("d/m/Y", strtotime($timestamp)) ?></span>
-	<span class="text-light text-right"><?php echo $ip ?></span>
-</div>
